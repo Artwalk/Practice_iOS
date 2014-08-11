a948b9bc7b0bc7c43d670fceeef8c0902561e79c
Artwalk <frank.holy.art@gmail.com>
2014-08-11 14:22:40 +0800

    finish: 7.2

---
+++ 7.2.AccessingVariablesinBlockObjects/7.2.AccessingVariablesinBlockObjects/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  7.2.AccessingVariablesinBlockObjects
+//
+//  Created by artwalk on 8/11/14.
+//  Copyright (c) 2014 artwalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

2a0c7c2269f2156c987d66520dfd40c683ab70ca
Artwalk <frank.holy.art@gmail.com>
2014-08-16 13:54:19 +0800

    11.3

---
+++ iOS 7 Programming Cookbook/11.3.Synchronously/11.3.SynchronouslyTests/_1_3_SynchronouslyTests.m
@@ -0,0 +1,34 @@
+//
+//  _1_3_SynchronouslyTests.m
+//  11.3.SynchronouslyTests
+//
+//  Created by artwalk on 8/16/14.
+//  Copyright (c) 2014 artwalk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _1_3_SynchronouslyTests : XCTestCase
+
+@end
+
+@implementation _1_3_SynchronouslyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

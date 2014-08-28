d5665da4add2fcbb33a16ec6d6a3ea29c53b145c
Artwalk <frank.holy.art@gmail.com>
2014-08-28 17:54:22 +0800

    20.12

---
+++ iOS 7 Programming Cookbook/20.11.Moving Shapes/20.11.Moving Shapes/view.h
@@ -0,0 +1,13 @@
+//
+//  view.h
+//  20.11.Moving Shapes
+//
+//  Created by artwalk on 8/28/14.
+//  Copyright (c) 2014 artwalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface view : UIView
+
+@end
---
+++ iOS 7 Programming Cookbook/20.11.Moving Shapes/20.11.Moving ShapesTests/_0_11_Moving_ShapesTests.m
@@ -0,0 +1,34 @@
+//
+//  _0_11_Moving_ShapesTests.m
+//  20.11.Moving ShapesTests
+//
+//  Created by artwalk on 8/28/14.
+//  Copyright (c) 2014 artwalk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0_11_Moving_ShapesTests : XCTestCase
+
+@end
+
+@implementation _0_11_Moving_ShapesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

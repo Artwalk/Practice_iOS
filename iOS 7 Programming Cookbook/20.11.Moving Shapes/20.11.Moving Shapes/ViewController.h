75717ea4549151c3fb28e1375e94909300f5247c
Artwalk <frank.holy.art@gmail.com>
2014-08-28 17:54:22 +0800

    20.12

---
+++ iOS 7 Programming Cookbook/20.11.Moving Shapes/20.11.Moving Shapes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  20.11.Moving Shapes
+//
+//  Created by artwalk on 8/28/14.
+//  Copyright (c) 2014 artwalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iOS 7 Programming Cookbook/20.11.Moving Shapes/20.11.Moving Shapes/view.m
@@ -0,0 +1,58 @@
+//
+//  view.m
+//  20.11.Moving Shapes
+//
+//  Created by artwalk on 8/28/14.
+//  Copyright (c) 2014 artwalk. All rights reserved.
+//
+
+#import "view.h"
+
+@implementation view
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+    CGMutablePathRef path = CGPathCreateMutable();
+    
+    CGRect rectangle = CGRectMake(100, 100, 150, 50);
+    
+    CGAffineTransform transform = CGAffineTransformMakeRotation((45 * M_PI) /180);
+    CGPathAddRect(path, &transform, rectangle);
+//    CGPathAddRect(path, NULL, rectangle);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSaveGState(context);
+    
+//    CGContextTranslateCTM(context, 100, 0);
+//    CGContextScaleCTM(context, 0.5, 0.5);
+    
+    CGContextAddPath(context, path);
+    
+    [[UIColor blueColor] setFill];
+    
+    [[UIColor redColor] setStroke];
+    CGContextSetLineWidth(context, 5.0f);
+    
+    CGContextDrawPath(context, kCGPathFillStroke);
+    
+    CGPathRelease(path);
+    
+    CGContextRestoreGState(context);
+}
+
+
+@end

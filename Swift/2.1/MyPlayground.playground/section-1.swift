1accbf14d23bb4008e018f088b880395a48a98a4
Artwalk <frank.holy.art@gmail.com>
2014-08-31 13:27:50 +0800

    2.1

--- Swift/2.1/MyPlayground.playground/section-1.swift
+++ Swift/2.1/MyPlayground.playground/section-1.swift
@@ -22,3 +22,36 @@ UInt64.max
 Int64.max
 Int.max
 Int32.max
+
+
+let possNum = "1356"
+let convNum = possNum.toInt()
+if (convNum != nil) {
+    println("\(convNum), hehe")
+}
+
+var i:Int? = 1
+println("\(i!)")
+
+var s:String! = "hehe"
+println(s)
+
+let dict = ["1":"324", "2":"325"]
+
+for (k, v) in dict {
+    println(k + ":" + v)
+}
+
+var ss = ["aaa", "bbb", "ccc"]
+sorted( ss, > )
+
+for i in 0...2 {
+    i + 1
+}
+
+__FILE__
+__LINE__
+__FUNCTION__
+
+let a = "a"
+a as String
